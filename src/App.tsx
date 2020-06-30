415ca90180c87c0f35fe3a53d008a8a3fb6e62f8
Guy Kremnitzer <guy.kremnitzer@salto.io>
2020-06-30 11:58:10 +0300

    install and use chakra-ui and theme-ui types

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React from 'react'
+import { Button } from '@chakra-ui/core'
 import logo from './logo.svg';
 import './App.css';
 
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Button />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
